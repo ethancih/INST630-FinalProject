ba2cc867150eb8c4c8b3b1c65ee0aa6ff7f51b36
EthanCIH <3than4ever@gmail.com>
2024-12-02 14:17:08 -0500

    added images

--- animation.js
+++ animation.js
@@ -3,10 +3,20 @@ console.log('Loaded animation.js');
 const weatherDetermine = await import("./weatherDetermine.js");
 const weatherData = await import("./weatherData.js");
 
-const backgrounds = [ ["./assets/backgrounds/sunny-01.jpg"],
-                      ["./assets/backgrounds/rainy-02.jpg"], 
-                      ["./assets/backgrounds/cloudy-02.jpg"],
-                      ["./assets/backgrounds/snowy-01.jpg"] ];
+const backgrounds = [ ["./assets/backgrounds/sunny-01.jpg", "./assets/backgrounds/sunny-02.jpg", "./assets/backgrounds/sunny-03.jpg"],
+                      ["./assets/backgrounds/rainy-02.jpg", "./assets/backgrounds/rainy-03.jpg", "./assets/backgrounds/rainy-04.jpg"], 
+                      ["./assets/backgrounds/cloudy-02.jpg", "./assets/backgrounds/cloudy-03.jpg", "./assets/backgrounds/cloudy-04.jpg"],
+                      ["./assets/backgrounds/snowy-01.jpg", "./assets/backgrounds/snowy-02.jpg", "./assets/backgrounds/snowy-03.jpg"] ];
+let bgCounter = [0, 0, 0, 0];
+function iterateBgCounter(currWeather) {
+  if (bgCounter[currWeather] <= 2) {
+    bgCounter[currWeather] += 1;
+  }
+  else {
+    bgCounter[currWeather] = 0;
+  }
+  console.log("bgCounter = " + bgCounter);
+}
 
 export async function defaultAnimation() {
   console.log("defaultAnimation EXECUTED");
@@ -15,8 +25,9 @@ export async function defaultAnimation() {
   const currWeather = await weatherDetermine.waitForWeather();
   console.log("currWeather = " + currWeather);
 
-  document.getElementById("background1").style.backgroundImage = "url('"+ backgrounds[currWeather] +"')";
-  document.getElementById("background2").style.backgroundImage = "url('"+ backgrounds[currWeather] +"')";
+  document.getElementById("background1").style.backgroundImage = "url('"+ backgrounds[currWeather][bgCounter[currWeather]] +"')";
+  document.getElementById("background2").style.backgroundImage = "url('"+ backgrounds[currWeather][bgCounter[currWeather]] +"')";
+  iterateBgCounter(currWeather);
 
   gsap.from(".weatherContainer", 1.8, { x: "100vw", ease: "power4.out", delay: 0 });
   gsap.from(".selectBox", 1.8, { x: "100vw", ease: "power4.out", delay: 0.3 });
@@ -52,12 +63,13 @@ export async function transAnimation() {
   tl.from("button", 1.8, { x: "100vw", ease: "power4.out" }, '<0.1');
 
   function updateBG1() {
-    document.getElementById("background1").style.backgroundImage = "url('"+ backgrounds[currWeather] +"')";
+    document.getElementById("background1").style.backgroundImage = "url('"+ backgrounds[currWeather][bgCounter[currWeather]] +"')";
     console.log("updated BG1");
   }
 
   function updateBG2() {
-    document.getElementById("background2").style.backgroundImage = "url('"+ backgrounds[currWeather] +"')";
+    document.getElementById("background2").style.backgroundImage = "url('"+ backgrounds[currWeather][bgCounter[currWeather]] +"')";
+    iterateBgCounter(currWeather);
     console.log("animation complete");
   }
 
